3578ba57974830467ad1542f7dc30011ae4d0ca7
4jip <Jipper2007@gmail.com>
2025-03-19 00:10:37 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 loadstring(
-    game:HttpGet("https://github.com/4jip/dahoodminecraftTexture/blob/main/main.lua") .. ";print('texture done');" ..
+    game:HttpGet("https://raw.githubusercontent.com/4jip/dahoodminecraftTexture/refs/heads/main/main.lua") .. ";print('texture done');" ..
     game:HttpGet("https://raw.githubusercontent.com/4jip/robloxforcefieldbody/refs/heads/main/main.lua") .. ";print('forcefield body done');" ..
     game:HttpGet("https://raw.githubusercontent.com/4jip/dans-gui/refs/heads/main/main.lua") .. ";print('dans gui done');"
 )()
